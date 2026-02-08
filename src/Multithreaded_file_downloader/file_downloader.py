bd250cd3fc45529347709e552ee3d607010f3294
Jaspinder Kaur <jaspinderkaurwalia855@gmail.com>
2026-02-08 12:27:25 +0530

    Modified file_downloader.py with proper comments

--- src/Multithreaded_file_downloader/file_downloader.py
+++ src/Multithreaded_file_downloader/file_downloader.py
@@ -3,24 +3,27 @@ import logging
 import os
 import urllib.request
 
-progress = {}              # Per-thread progress tracking
-lock = threading.Lock()    # Thread safety
+# dictionary to track download progress
+progress = {}    
+# thread safety lock
+lock = threading.Lock()    
 
 # Function to download file
 def download_file(url, filename, thread_id):
     try:
+        #logs when a thread starts downloading
         with lock:
             logging.info(f"Thread {thread_id} STARTED downloading {os.path.basename(filename)}")
             progress[thread_id] = 0
-
+        # function to update and calculate progress
         def reporthook(block_num, block_size, total_size):
             if total_size > 0:
                 percent = min(100, (block_num * block_size) / total_size * 100)
                 with lock:
                     progress[thread_id] = percent
-
+        # downloading the file and tracking progress using reporthook function
         urllib.request.urlretrieve(url, filename, reporthook)
-
+        # updating after completion of download
         with lock:
             progress[thread_id] = 100
             logging.info(f"Thread {thread_id} FINISHED downloading {os.path.basename(filename)}")
