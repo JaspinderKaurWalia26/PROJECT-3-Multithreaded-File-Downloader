a49f8d63edb8c100f73a0c246278f6dc7d256827
Jaspinder Kaur <jaspinderkaurwalia855@gmail.com>
2026-02-08 12:28:36 +0530

    Modified main.py with proper convention and comments

--- src/Multithreaded_file_downloader/main.py
+++ src/Multithreaded_file_downloader/main.py
@@ -4,11 +4,13 @@ import os
 from .file_downloader import download_file
 from .logger import setup_logger
 
-
+# logging configuration setup
 setup_logger()
+# download directory path
 DOWNLOAD_DIR="data/downloads"
 # Main function
 def main():
+    # all urls with file names
     urls = [
         ("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf", "file1.pdf"),
         ("https://raw.githubusercontent.com/dwyl/english-words/master/words.txt", "file2.txt"),
@@ -21,19 +23,23 @@ def main():
         ("https://raw.githubusercontent.com/v0re/dirb/master/wordlists/common.txt", "file9.txt"),
         ("https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english.txt", "file10.txt"),
     ]
-
+    # list to keep track of threads
     threads = []
-
+    
     for idx, (url, filename) in enumerate(urls):
+        # full path where files will be downloaded
         file_path = os.path.join(DOWNLOAD_DIR, filename)
-        t = threading.Thread(target=download_file, args=(url, file_path, idx))
-        threads.append(t)
-        t.start()
+        # creating a new thread for each file
+        thread = threading.Thread(target=download_file, args=(url, file_path, idx))
+        # adding thread to the thread list
+        threads.append(thread)
+        # starting thread
+        thread.start()
         
 
     # Wait for all threads
-    for t in threads:
-        t.join()
+    for thread in threads:
+        thread.join()
 
     logging.info("All threads completed.")
 
